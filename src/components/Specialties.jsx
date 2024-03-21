bd8232741bf1e2d1aa64d39ea130041fb9d124a1
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-03-21 22:00:28 +0530

    home page ui completed

---
+++ src/components/Specialties.jsx
@@ -0,0 +1,35 @@
+import { IoIosArrowDropright, IoIosArrowDropleft } from "react-icons/io";
+function Specialties() {
+  return (
+    <div className="bg-[#EBF5FF] p-8 sm:p-20">
+      <div className="flex justify-center">
+        <h1 className="text-xl sm:text-3xl font-bold underline mb-5">
+          Specialties
+        </h1>
+      </div>
+      <div className="flex justify-center">
+      <div className="items-center flex">
+          <IoIosArrowDropleft sm:size={30} size={25} style={{ color: '#949494' }}/>
+        </div>
+        <div className="flex gap-10 bg-white p-7 md:h-[200px] md:w-11/12 sm:gap-14 rounded-xl overflow-x-scroll scroll-smooth scrollbar-hide">
+          {[...Array(10)].map((_, index) => (
+            <div
+              key={index}
+              className="flex-none text-center w-3/12 md:mx-12 md:w-1/12"
+            >
+              <img className="w-full sm:w-40" src="/heart.png" alt="" />
+              <p className="font-semibold font-mono text-lg text-[10px] sm:text-[18px]">
+                rokham
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="items-center flex">
+          <IoIosArrowDropright sm:size={30} size={25} style={{ color: '#949494' }}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Specialties;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Specialties from "../components/Specialties";
 
 function Home() {
   return (
@@ -41,6 +43,87 @@ function Home() {
           </div>
         </div>
       </div>
+
+      {/* features */}
+      <div className="bg-blue-50 py-2 sm:py-3 md:py-3 lg:py-3 xl:py-3">
+        <div className="mx-4 flex flex-wrap justify-center">
+          <div className="flex flex-col space-y-8 sm:space-y-0 sm:flex-row sm:space-x-8 overflow-x-auto">
+            {/* Certified Doctors */}
+            <div className="w-full sm:w-96 h-64 duration-500 group overflow-hidden relative rounded-2xl bg-neutral-800 text-neutral-50 p-4 flex flex-col justify-evenly">
+              <div className="absolute blur duration-500 group-hover:blur-none w-72 h-72 rounded-full group-hover:translate-x-12 group-hover:translate-y-12 bg-sky-900 right-1 -bottom-24"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-12 h-12 rounded-full group-hover:translate-x-12 group-hover:translate-y-2 bg-indigo-700 right-12 bottom-12"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-36 h-36 rounded-full group-hover:translate-x-12 group-hover:-translate-y-12 bg-indigo-800 right-1 -top-12"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-24 h-24 bg-sky-700 rounded-full group-hover:-translate-x-12"></div>
+              <div className="z-10 flex flex-col justify-evenly w-full h-full">
+                <span className="text-2xl font-bold">Certified Doctors</span>
+                <p>
+                  We offer quality healthcare through our network of certified
+                  and experienced doctors.
+                </p>
+                <div className="flex justify-center">
+                  <img className="w-24 h-24" src="certified.svg" alt="" />
+                </div>
+              </div>
+            </div>
+
+            {/* 100% Confidential */}
+            <div className="w-full sm:w-96 h-64 duration-500 group rounded-2xl overflow-hidden relative bg-neutral-800 text-neutral-50 p-4 flex flex-col justify-evenly">
+              <div className="absolute blur duration-500 group-hover:blur-none w-72 h-72 rounded-full group-hover:translate-x-12 group-hover:translate-y-12 bg-sky-900 right-1 -bottom-24"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-12 h-12 rounded-full group-hover:translate-x-12 group-hover:translate-y-2 bg-indigo-700 right-12 bottom-12"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-36 h-36 rounded-full group-hover:translate-x-12 group-hover:-translate-y-12 bg-indigo-800 right-1 -top-12"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-24 h-24 bg-sky-700 rounded-full group-hover:-translate-x-12"></div>
+              <div className="z-10 flex flex-col justify-evenly w-full h-full">
+                <span className="text-2xl font-bold">100% Confidential</span>
+                <p>
+                  All advice & consultations are completely confidential. You
+                  can also delete chats whenever you want.
+                </p>
+                <div className="flex justify-center">
+                  <img className="w-24 h-24" src="confidential.svg" alt="" />
+                </div>
+              </div>
+            </div>
+
+            {/* Convenience */}
+            <div className="w-full sm:w-96 h-64 duration-500 group overflow-hidden relative rounded-2xl bg-neutral-800 text-neutral-50 p-4 flex flex-col justify-evenly">
+              <div className="absolute blur duration-500 group-hover:blur-none w-72 h-72 rounded-full group-hover:translate-x-12 group-hover:translate-y-12 bg-sky-900 right-1 -bottom-24"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-12 h-12 rounded-full group-hover:translate-x-12 group-hover:translate-y-2 bg-indigo-700 right-12 bottom-12"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-36 h-36 rounded-full group-hover:translate-x-12 group-hover:-translate-y-12 bg-indigo-800 right-1 -top-12"></div>
+              <div className="absolute blur duration-500 group-hover:blur-none w-24 h-24 bg-sky-700 rounded-full group-hover:-translate-x-12"></div>
+              <div className="z-10 flex flex-col justify-evenly w-full h-full">
+                <span className="text-2xl font-bold">Convenience</span>
+                <p>
+                  Forget the hassle of long queues and rush hour. Seek expert
+                  opinion anytime, anywhere.
+                </p>
+                <div className="flex justify-center">
+                  <img className="w-24 h-24" src="convenience.svg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Specialties />
+      <div className="flex flex-col bg-[#EBF5FF] items-center p-4 sm:mx-0 ">
+        <div className="bg-[#58A399] p-10 rounded-3xl">
+          <div className="flex justify-center mb-2">
+            <p className="text-white text-2xl font-bold antialiased">
+              Are you a Doctor?
+            </p>
+          </div>
+          <div className="flex justify-center mb-2">
+            <p className="text-white font-medium text-lg">
+              Join our panel of specialists and connect with your patients from
+              anywhere.
+            </p>
+          </div>
+          <div className="flex justify-center mb-2">
+            <button className="bg-slate-200 w-32 p-1 rounded">JOIN</button>
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </>
   );
 }
