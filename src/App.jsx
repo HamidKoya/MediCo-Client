240d179a7a0b39a99e815ae0a782995792b38f67
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-03-27 20:51:46 +0530

    shadcnconfiguration completed successfully

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Doctor from './pages/Doctor'
 import Signup from './pages/Signup'
+import Otp from './pages/Otp'
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/doctors' element={<Doctor/>}/>
       <Route path='/signup' element={<Signup/>}/>
+      <Route path='/otp' element={<Otp/>}/>
     </Routes>
   )
 }
