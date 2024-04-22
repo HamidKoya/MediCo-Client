9180a7600fee8f3eccb73202988bf20ea0bc76c3
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-04-22 15:54:31 +0530

    console removed

--- src/pages/admin/DoctorDetails.jsx
+++ src/pages/admin/DoctorDetails.jsx
@@ -8,7 +8,6 @@ import axios from "axios";
 
 function DoctorDetails() {
   const [doctorData, setDoctorData] = useState(null);
-  console.log(doctorData._id);
   const { id } = useParams();
 
   useEffect(() => {
