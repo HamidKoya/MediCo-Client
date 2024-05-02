eb67c8a526f5102836e2ea911ed9c52835593547
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-05-02 18:56:49 +0530

    no slots handled

--- src/pages/doctor/SlotsDoctor.jsx
+++ src/pages/doctor/SlotsDoctor.jsx
@@ -56,72 +56,86 @@ function SlotsDoctor() {
   return (
     <div>
       <Header />
-      <div className="bg-blue-50 w-full h-full p-4">
-        <h1 className="text-xl font-semibold flex justify-center py-4">
-          Slots List
-        </h1>
-        <div className="mx-auto sm:p-8 mb-10 bg-[#71A0A8]">
-          <Table>
-            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-            <TableHeader>
-              <TableRow>
-                <TableHead className="text-center w-[100px] text-blue-700">
-                  No
-                </TableHead>
-                <TableHead className="text-center text-blue-700">
-                  Date
-                </TableHead>
-                <TableHead className="text-center text-blue-700">
-                  Starting Time
-                </TableHead>
-                <TableHead className="text-center text-blue-700">
-                  Ending Time
-                </TableHead>
-                <TableHead className=" text-blue-700 text-center">
-                  Duration
-                </TableHead>
-                <TableHead className="text-center text-blue-700">
-                  View Slots
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {paginatedSlots.map((slot, index) => (
-                <TableRow key={slot._id}>
-                  <TableCell className="font-medium text-center">
-                    {(currentPage - 1) * pageSize + index + 1}
-                  </TableCell>
-                  <TableCell className="text-center">
-                    {new Date(slot.date).toLocaleDateString()}
-                  </TableCell>
-                  <TableCell className="text-center">
-                    {slot.startTime}
-                  </TableCell>
-                  <TableCell className="text-center">{slot.endTime}</TableCell>
-                  <TableCell className="text-center">
-                    {slot.slotDuration}
-                  </TableCell>
-                  <TableCell className="text-center">
-                    {" "}
-                    <button onClick={()=>handleClick(slot._id)} className="bg-green-400 p-1 rounded-md h-7">
-                      Details
-                    </button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell colSpan={5} className="text-blue-700">
-                  Total Slots
-                </TableCell>
-                <TableCell className="text-center text-blue-700">
-                  {slots.length}
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </div>
+      <div className={`bg-blue-50 w-full p-4 ${paginatedSlots.length === 0?'h-[600px]':'h-full'}`}>
+        {paginatedSlots.length === 0 ? (
+          <div className="text-center py-6">
+            <p className="text-gray-500 text-lg">No slots added</p>
+          </div>
+        ) : (
+          <>
+            <h1 className="text-xl font-semibold flex justify-center py-4">
+              Slots List
+            </h1>
+            <div className="mx-auto sm:p-8 mb-10 bg-[#71A0A8]">
+              <Table>
+                {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="text-center w-[100px] text-blue-700">
+                      No
+                    </TableHead>
+                    <TableHead className="text-center text-blue-700">
+                      Date
+                    </TableHead>
+                    <TableHead className="text-center text-blue-700">
+                      Starting Time
+                    </TableHead>
+                    <TableHead className="text-center text-blue-700">
+                      Ending Time
+                    </TableHead>
+                    <TableHead className=" text-blue-700 text-center">
+                      Duration
+                    </TableHead>
+                    <TableHead className="text-center text-blue-700">
+                      View Slots
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {paginatedSlots.map((slot, index) => (
+                    <TableRow key={slot._id}>
+                      <TableCell className="font-medium text-center">
+                        {(currentPage - 1) * pageSize + index + 1}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {new Date(slot.date).toLocaleDateString()}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {slot.startTime}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {slot.endTime}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {slot.slotDuration}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {" "}
+                        <button
+                          onClick={() => handleClick(slot._id)}
+                          className="bg-green-400 p-1 rounded-md h-7"
+                        >
+                          Details
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TableCell colSpan={5} className="text-blue-700">
+                      Total Slots
+                    </TableCell>
+                    <TableCell className="text-center text-blue-700">
+                      {slots.length}
+                    </TableCell>
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </div>
+          </>
+        )}
+
         {slots && pageSize && (
           <div className="flex justify-center mt-4 bg-blue-50">
             {Array.from(
@@ -143,7 +157,11 @@ function SlotsDoctor() {
           </div>
         )}
 
-        <Modal className="max-w-xl max-h-[500px] bg-[#71A0A8] mx-auto mt-14" show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal
+          className="max-w-xl max-h-[500px] bg-[#71A0A8] mx-auto mt-14"
+          show={openModal}
+          onClose={() => setOpenModal(false)}
+        >
           <Modal.Header className="bg-white ">Time Slots</Modal.Header>
           <Modal.Body>
             {selectedSlot && (
