9426d20811436a8ad33d1bdb368afaf4cf61c869
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-05-03 21:54:54 +0530

    booking initial stage completed

--- src/pages/user/DoctorDetailsPage.jsx
+++ src/pages/user/DoctorDetailsPage.jsx
@@ -4,8 +4,14 @@ import Footer from "@/components/user/Footer";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Button, Modal } from 'flowbite-react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Swal from 'sweetalert2';
 function DoctorDetailsPage() {
   const [doctor, setDoctor] = useState();
+  const [openModal, setOpenModal] = useState(false);
+  const [slots, setSlots] = useState([]);
   const { id } = useParams();
   useEffect(() => {
     axios
@@ -18,6 +24,48 @@ function DoctorDetailsPage() {
         console.log(error.message);
       });
   }, []);
+
+  const handleChange = async (date) => {
+    try {
+      
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0);
+
+        if (date < currentDate) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Invalid Date',
+                text: 'Please select a date in the future.',
+            });
+            setSelect(null);
+            setSlots([]);
+            return;
+        }
+        setDate(date);
+        console.log('from frontend');
+        const response = await axios.get(`http://localhost:3000/slotList?id=${id}&date=${date}`)
+
+        const availableSlots = response?.data?.availableSlots;
+
+        if (availableSlots && availableSlots.length > 0) {
+            let allAvailableSlots = [];
+            availableSlots.forEach(slot => {
+                slot?.timeSlots?.forEach(timeSlot => {
+                    if (timeSlot.booked === false) {
+                        allAvailableSlots.push(timeSlot);
+                    }
+                });
+            });
+            setSlots(allAvailableSlots);
+        } else {
+            setSlots([]);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
   return (
     <div>
       <Header2 />
@@ -25,7 +73,13 @@ function DoctorDetailsPage() {
         <div class="grid sm:grid-cols-2 grid-cols-1 gap-4 my-12">
           <div class="w-[350px] sm:w-[400px] h-[500px] bg-white shadow-2xl shadow-slate-500 rounded-2xl">
             <div className="flex flex-col justify-center items-center mt-10 gap-3 mb-3">
-             {doctor&&(<img src={doctor.photo} alt="" className="w-36 h-36 rounded-full" />)} 
+              {doctor && (
+                <img
+                  src={doctor.photo}
+                  alt=""
+                  className="w-36 h-36 rounded-full"
+                />
+              )}
               {doctor && (
                 <p className="text-xl font-medium">Dr.{` ${doctor.name}`}</p>
               )}
@@ -46,7 +100,10 @@ function DoctorDetailsPage() {
               {doctor && <p>Experience:{` ${doctor.experience}`}</p>}
             </div>
             <div className="flex justify-center mt-4">
-              <button className="bg-teal-400 p-2 text-sm font-semibold text-white rounded-lg active:scale-90">
+              <button
+                onClick={() => setOpenModal(true)}
+                className="bg-teal-400 p-2 text-sm font-semibold text-white rounded-lg active:scale-90"
+              >
                 View Slots
               </button>
             </div>
@@ -58,6 +115,82 @@ function DoctorDetailsPage() {
           </div>
         </div>
       </div>
+
+      <Modal show={openModal} onClose={() => setOpenModal(false)} className="max-w-[650px] mx-auto mt-14">
+        <Modal.Header>Select Date and Time</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <label htmlFor="datePicker">Select Date:</label>
+            <div className="flex justify-center">
+              <Calendar onChange={(date) => handleChange(date)} />
+            </div>
+            <p>Available Slots:</p>
+            {slots.length > 0 ? (
+              <ul className="flex flex-wrap justify-center">
+                {slots.map((slot, index) => {
+                  const slotStartTime = slot.start;
+                  const currentDate = new Date();
+                  let isSlotDisabled = false;
+
+                  if (date < currentDate) {
+                    if (slotStartTime < currentTime) {
+                      isSlotDisabled = true;
+                    }
+                  }
+
+                  return (
+                    <li
+                      key={index}
+                      className={`flex items-center h-10 w-36 m-2 p-2 rounded-xl text-white ${
+                        select === slot.objectId ? "bg-green-500" : ""
+                      } ${
+                        isSlotDisabled
+                          ? "bg-gray-300 cursor-not-allowed"
+                          : "bg-blue-500 hover:bg-blue-700"
+                      }`}
+                    >
+                      <input
+                        type="checkbox"
+                        id={`slot-${index}`}
+                        className="mr-2"
+                        checked={select === slot.objectId}
+                        onChange={() => handleSelect(slot.objectId)}
+                        disabled={isSlotDisabled}
+                      />
+                      <label
+                        htmlFor={`slot-${index}`}
+                        className="flex-1"
+                      >{`${slot.start} - ${slot.end}`}</label>
+                    </li>
+                  );
+                })}
+              </ul>
+            ) : (
+              <p className="text-orange-600">
+                No available slots for the selected date.
+              </p>
+            )}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          {slots.length > 0 && select && (
+            <div className="">
+              <button
+                className="btn btn-outline btn-primary"
+                onClick={handlePayment}
+              >
+                ONLINE PAYMENT
+              </button>
+              <button
+                className="btn btn-outline btn-warning mx-5"
+                onClick={walletPay}
+              >
+                WALLET PAY
+              </button>
+            </div>
+          )}
+        </Modal.Footer>
+      </Modal>
 
       <Footer />
     </div>
