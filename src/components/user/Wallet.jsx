dabc0bbc9419615aeb9deee7808503f368429fcc
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-05-09 19:44:07 +0530

    functionality changed for wallet amount

--- src/components/user/Wallet.jsx
+++ src/components/user/Wallet.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { useSelector } from "react-redux";
+import { useEffect,useState } from "react";
+import axios from "axios";
 function Wallet() {
     const {currentUser} = useSelector((state)=>state.user)
+    const [wallet,setWallet] = useState()
+    const userId = currentUser.userData._id
+    useEffect(()=>{
+        axios.post("http://localhost:3000/wallet",{userId}).then((res)=>{
+            setWallet(res.data)
+        }).catch((error)=>{
+            console.log(error.message);
+        })
+    },[userId,wallet])
   return (
     <div>
       <Dialog>
@@ -16,7 +27,7 @@ function Wallet() {
             <div className="flex justify-center">
               <img className="w-10 h-10" src="/wallet.png" alt="wallet" />
             </div>
-            <p className="flex justify-center mt-4 font-medium text-green-400">Balance : ₹ {currentUser.userData.wallet}</p>
+            <p className="flex justify-center mt-4 font-medium text-green-400">Balance : ₹ {wallet}</p>
           </div>
         </DialogContent>
       </Dialog>
