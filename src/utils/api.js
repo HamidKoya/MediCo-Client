c57db4ebaf8554b63fada4491f9ceafdf966f942
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-06-08 16:46:38 +0530

    api changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
-const api = axios.create({baseURL:`http://localhost:3000`,withCredentials:true})
+// const api = axios.create({baseURL:`http://localhost:3000`,withCredentials:true})
+const api = axios.create({baseURL:`https://medico-server-b7s5.onrender.com`,withCredentials:true})
 
 export default api
