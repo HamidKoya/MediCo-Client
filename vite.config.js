107db9144eff5f1dff3389af9f8be4d9f3ce8856
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-06-25 15:29:39 +0530

    vite configue changed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from "path"
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(),],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, "index.html"),
+            },
+        },
+    },
+});
