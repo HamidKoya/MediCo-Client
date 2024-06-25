25c64b06dddd92605740d9e1f57e4cdba9766b91
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-06-25 16:23:12 +0530

    unwanted comment deleted

--- vite.config.js
+++ vite.config.js
@@ -11,19 +11,3 @@ export default defineConfig({
     },
   },
 })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [react()],
-//     build: {
-//         rollupOptions: {
-//             input: {
-//                 main: path.resolve(__dirname, "index.html"),
-//             },
-//         },
-//     },
-// });
